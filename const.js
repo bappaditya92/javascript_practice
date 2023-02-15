eee601d8256d02791c9797c0cab2ff629d6ef4df
unknown <bappadityasinha.92@gmail.com git config --global user.name bappaditya92>
2023-02-16 00:26:00 +0530

    third commit

---
+++ const.js
@@ -0,0 +1,37 @@
+//let age = 15;
+//console.log(age);
+
+// function sum(){
+//     let num1 = 10;
+//     let num2 = 100;
+//     const total = num1 + num2;
+//     return total;
+// }
+
+// console.log(sum());
+
+// for(var i=0; i<=10; i++){
+//     console.log("inside loop" + i);
+// }
+// console.log("outside loop" + i);
+
+
+// for(let i=0; i<=10; i++){
+//     console.log("inside loop" + i);
+// }
+// console.log("outside loop" + i);
+// var dob = "2nd nov";
+// var dob = "1st september";
+// let age = 20;
+// age = 52;//reasign possible
+// const name = "Bappaditya";
+// name = "Bappa"; //reassign is not possible
+// console.log(name)
+// console.log(age);
+// console.log(dob);
+
+const obj = {name: "Bappaditya"};
+//obj = {age: 20}; //reassign
+obj.name = "adi";
+//console.log(obj.name);
+console.log(obj.name);
